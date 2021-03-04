3b4a7cf5c998dca28d7c8ddcb27333f99dbc9797
Youness B <11468624+blfunex@users.noreply.github.com>
2021-03-04 13:27:47 +0100

    WIP: Added some type definitions for models

---
+++ src/models/PokeAPI.ts
@@ -0,0 +1,41 @@
+export interface ResponseReference {
+  name: string;
+  url: string;
+}
+
+interface PokemonResponse {
+  // ...
+  id: string;
+  name: string;
+  species: ResponseReference[];
+  sprites: {
+    back_default: null | string;
+    front_default: null | string;
+  }[];
+  types: PokemonResponse.PokemonType[];
+}
+
+declare namespace PokemonResponse {
+  export interface PokemonType {
+    slot: number;
+    type: ResponseReference[];
+  }
+}
+
+export type { PokemonResponse };
+
+export interface PokemonSpeciesResponse {
+  // ...
+  flavor_text_entries: {
+    flavor_text: string;
+    language: ResponseReference;
+    version: ResponseReference;
+  }[];
+}
+
+export interface PokemonsResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  result: ResponseReference[];
+}
---
+++ src/models/Pokemon.ts
@@ -0,0 +1,22 @@
+export default interface Pokemon {
+  id: string;
+  name: string;
+  generationId: string;
+  sprites: [front: string | null, back: string | null];
+  types: string[];
+}
+
+export function getGenerationRomanNumeral(generationId: string) {
+  return generationId.slice(11).toUpperCase();
+}
+
+export const GenerationHumanizedText: Record<string, string> = {
+  "generation-i": "first generation",
+  "generation-ii": "second generation",
+  "generation-iii": "third generation",
+  "generation-iv": "fourth generation",
+  "generation-v": "fifth generation",
+  "generation-vi": "sixth generation",
+  "generation-vii": "seventh generation",
+  "generation-viii": "eighth generation",
+};
